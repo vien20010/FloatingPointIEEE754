fd7a978695de679d1a5b765a52c1fff733a761d9
LV_Embeddded <nguyenlamvien0110@gmail.com>
2020-12-25 02:27:24 +0700

    add comment

--- Multiple/mult.sv
+++ Multiple/mult.sv
@@ -43,21 +43,23 @@ mult24 mul_significands(fracA,fracB,temp_fracCPA);
 assign frac_out=temp_fracCPA[46:23]; //Lấy 24 bit frac sau phép nhân 24 bit
 //Tìm vị trí bit 1 đầu tiên để chuẩn hóa
 find1 findbit1(.in(frac_out), .nshiftleft(nshiftleft));
-//Dịch trái 24 bit frac về dạng 1.xxx.....
+//Dịch trái 24 bit frac về dạng 1.xxx..... nếu frac_out có dạng 1,.... hoặc 0,.....
 shiftleft24 shift_fracl(.in(frac_out), .nshiftleft(nshiftleft), .out(fracleft_before_result));
 //Dịch phải nếu nó là dạng 10,....
 shift_right shift_fracr(.in(frac_out), .nshift(5'b1), .out(fracright_before_result));
 
 assign frac_result = temp_fracCPA[47]?fracright_before_result[22:0]:fracleft_before_result[22:0];
-
+//Tính toán cộng thêm hay trừ đi exponent
 compliment2_10bit compli2_exponent(.in({5'b0,nshiftleft}),.out(shiftleftout));
-
+//Nếu có dạng 10,.... thì 
 assign exp_add = temp_fracCPA[47]?10'b1:shiftleftout;
 adder_10bit	add_2(.in1(exp_add), .in2({2'b00,exp_out}), .S(temp_exp), .Cout());
 
+//Xét xem có bị underflow hay overflow hay không
 assign underflow = temp_exp[9]&temp_exp[8];
 assign overflow = ~temp_exp[9]&(temp_exp[8]|(&({temp_exp[7:0]})));
 
+//Xuất kết quả exponent
 assign exp_result = temp_exp[8]?8'd0:temp_exp[7:0];
 
 export_result export1(.in1(A), .in2(B), .temp_result({sign_result,exp_result,frac_result}), .result(result));
